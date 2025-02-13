e65dce680cc3a912e880396faa47c414efb5237d
MakiDizajnerica <n.marijanovic@hotmail.com>
2025-02-13 16:22:56 +0100

    added array mergeByAppending method

--- src/Mixins/ArrMixin.php
+++ src/Mixins/ArrMixin.php
@@ -4,5 +4,26 @@ namespace MakiDizajnerica\Expanded\Mixins;
 
 class ArrMixin
 {
-    //
+    /**
+     * Merge two arrays by appending one array keys to other array values.
+     *
+     * @param  array $toAppendOn
+     * @param  array $toAppend
+     * @param  bool  $returnDefault
+     * @param  mixed $default
+     * @return array
+     */
+    public static function mergeByAppending(array $toAppendOn,
+                                            array $toAppend,
+                                            bool $returnDefault = false,
+                                            mixed $default = null): array
+    {
+        return array_map(function ($value) use ($toAppend, $returnDefault, $default) {
+            return static::get(
+                $toAppend, $value, $returnDefault
+                    ? $default
+                    : $value
+            );
+        }, $toAppendOn);
+    }
 }
